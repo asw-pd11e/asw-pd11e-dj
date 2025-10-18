0a0fe160e7cd6f5fed1dd36314fc6cbf97a78de8
PathologicalMiss <toni.gratacos.miralles@gmail.com>
2025-10-18 12:38:58 +0200

    Add Render domain to ALLOWED_HOSTS

--- asw_pd11e_dj/settings.py
+++ asw_pd11e_dj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asw-pd11e-dj.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
